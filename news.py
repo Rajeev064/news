0fa2382b51d02419b77fd60c0bd0543ba5a05923
Rajeev064 <rs3167196@gmail.com>
2020-05-25 18:07:31 +0530

    Added a scheduler

--- news.py
+++ news.py
@@ -1,22 +1,24 @@
-
 import feedparser
 from win10toast import ToastNotifier
 import os
 from datetime import date
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 t = ToastNotifier()
 def Parsefeed():
-    c = input("Enter country :\n1.UK\n2.India")
-    if (c=='1' or c=='UK'):
+    c = input("Enter country :\n1.UK\n2.India\n3.Exit")
+    if (c == '1' or c=='UK'):
         f1 = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
         f = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
-    else : 
+    elif(c == '2' or c == 'India') : 
         f1 = feedparser.parse("https://feeds.feedburner.com/ndtvnews-top-stories")
         f = feedparser.parse("https://feeds.feedburner.com/ndtvnews-top-stories")
+    else:
+        os._exit
     ICON_PATH = os.getcwd() + "/icon.ico"
-    c=1
+    d=1
     for newsitem in f['items']:
-        if c<=10:
+        if d<=10:
             modified = f1.get('modified')
             if modified is not None:
                 print(modified)
@@ -26,22 +28,24 @@ def Parsefeed():
                 print('\n')
                 summary = modified  + '\n' + newsitem['summary']
                 t.show_toast(title,summary,ICON_PATH,duration = 10)
-                c = c+1
+                d = d+1
             else :
-                if c<=10:
-                    today = date.today()
-                    modified = today.strftime("%B %d, %Y")
-                    print(modified)
-                    print(newsitem['title'])
-                    title = newsitem['title']
-                    print(newsitem['summary'])
-                    print('\n')
-                    summary = modified  + '\n' + newsitem['summary']
-                    t.show_toast(title,summary,ICON_PATH,duration = 10)
-                    c = c+1
+                today = date.today()
+                modified = today.strftime("%B %d, %Y")
+                print(modified)
+                print(newsitem['title'])
+                title = newsitem['title']
+                print(newsitem['summary'])
+                print('\n')
+                summary = modified  + '\n' + newsitem['summary']
+                t.show_toast(title,summary,ICON_PATH,duration = 10)
+                d = d+1
 
 if __name__ == '__main__':
     try:
         Parsefeed()
+        scheduler = BlockingScheduler()
+        scheduler.add_job(Parsefeed,'interval',hour = 1)
+        scheduler.start()
     except:
         print("Error")
